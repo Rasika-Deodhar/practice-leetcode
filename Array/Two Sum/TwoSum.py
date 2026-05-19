630e8ad49ffa35abfe372df6feee221cc8b53aef
Rasika Deodhar <rasikadeodhar95@gmail.com>
2026-05-19 01:03:56 -0500

    Two sum solved

--- Array/Two Sum/TwoSum.py
+++ Array/Two Sum/TwoSum.py
@@ -1,1 +1,22 @@
-# code
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        k={}
+        currentSum=0
+        for i in range(len(nums)):
+            k[nums[i]]=i
+        print(k)
+        for i in range(len(nums)):
+            if target-nums[i] in k and k[target-nums[i]]!=i:
+                return[i,k[target-nums[i]]]
+
+        # n-square
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         currentSum=nums[i]+nums[j]
+        #         if currentSum==target:
+        #             return [i,j]
