e1a1a34a38f9b42b7618e5c397104f03bb20aff2
Rasika Deodhar <rasikadeodhar95@gmail.com>
2026-05-20 15:15:43 -0500

    longest common prefix solved

--- Array/Longest Common Prefix/LongestCommonPrefix.py
+++ Array/Longest Common Prefix/LongestCommonPrefix.py
@@ -1,1 +1,23 @@
-# code
+
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefixString = ""
+        j=0
+        minLengthIndex = min(range(len(strs)), key=lambda i:len(strs[i]))
+        while j<len(strs[minLengthIndex]):
+            currentString=""
+            for i in range(len(strs)):
+                if currentString == "":
+                    currentString+=strs[i][j]
+                if currentString[-1]!=strs[i][j]:
+                    return prefixString
+            prefixString+=currentString
+            j+=1
+        return prefixString
+        
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.longestCommonPrefix(["ab", "a"]))
