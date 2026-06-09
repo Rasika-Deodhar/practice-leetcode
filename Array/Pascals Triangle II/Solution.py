3415360e4cbc25a109f040a7cff976722e63b92f
Rasika Deodhar <rasikadeodhar95@gmail.com>
2026-06-08 22:32:14 -0500

    Pascal's Triangle II - Coding Ninja Solution

---
+++ Array/Pascals Triangle II/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        if rowIndex == 1:
+            return [1,1]
+        
+        result = []
+
+        leftZero = [0,1]
+        rightZero = [1,0]
+
+        for i in range(rowIndex):
+            result=[x+y for x,y in zip(leftZero,rightZero)]
+            leftZero = [0]+result
+            rightZero = result+[0]        
+
+        return result
