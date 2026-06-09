00169dfed58bf9d773743d07404becb6b0bad630
Rasika Deodhar <rasikadeodhar95@gmail.com>
2026-06-08 22:34:58 -0500

    slight changes

--- Array/Pascals Triangle II/Solution.py
+++ Array/Pascals Triangle II/Solution.py
@@ -2,8 +2,6 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         if rowIndex == 0:
             return [1]
-        if rowIndex == 1:
-            return [1,1]
         
         result = []
 
