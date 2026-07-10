354bd762a8a4cda9c8d69628368b4ea75c7f9050
Rasika Deodhar <rasikadeodhar95@gmail.com>
2026-07-09 23:06:33 -0500

    re-solved missing number

--- Array/Missing Number/Solution.py
+++ Array/Missing Number/Solution.py
@@ -1,11 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # 0 1 ... n-1
-
         nums.sort()
 
-        for i in range(len(nums)):
-            if i+1 not in nums:
-                return i+1
-        
-        return 0
+        for index, num in enumerate(nums):
+            if index!=num:
+                return index
+        return len(nums)
